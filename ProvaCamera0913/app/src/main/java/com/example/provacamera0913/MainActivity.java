b13670162ba02c7404e249f78f90f57c37813afa
marzna <annamarze99uni@gmail.com>
2021-09-16 12:37:20 +0200

    Creata parte di riconscimento volto conopenCV (per testare la CV)

--- ProvaCamera0913/app/src/main/java/com/example/provacamera0913/MainActivity.java
+++ ProvaCamera0913/app/src/main/java/com/example/provacamera0913/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,7 +20,18 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
@@ -27,38 +39,12 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     //private CameraBridgeViewBase javaCameraView;
     private JavaCameraView javaCameraView;
-
-    private Mat mRGBA, mRGBAT;
+    private File cascFile;
+    private Mat mRGBA, mRGBAT, mGrey;
 
     private static final int MY_CAMERA_REQUEST_CODE = 100; //Per la richiesta dei permessi della fotocamera
     int activeCamera = CameraBridgeViewBase.CAMERA_ID_FRONT; //Attiva la fotocamera frontale (?)
-
-    private BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(MainActivity.this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch(status) {
-                case BaseLoaderCallback.SUCCESS: {
-                    javaCameraView.enableView();
-                    break;}
-                default: {
-                    super.onManagerConnected(status);
-                    break;
-                }
-            }
-            super.onManagerConnected(status);
-        }
-    };
-
-    /**static {
-     if (OpenCVLoader.initDebug()) {
-     Log.d(TAG, "OpenCV is Configured or Connected Successfully.");
-     //Log.d(tag: TAG, msg: "OpenCV is Configured or Connected Successfully.");
-     } else{
-     Log.d(TAG, "OpenCV not Working or Loaded");
-     //Log.d(tag: TAG, msg: "OpenCV not Working or Loaded");
-     }
-     }
-     */
+    CascadeClassifier faceDetector;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -107,25 +93,41 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     public void onCameraViewStarted(int width, int height) {
         mRGBA = new Mat(height, width, CvType.CV_8UC4);
+        mGrey = new Mat();
     }
 
     @Override
     public void onCameraViewStopped() {
         mRGBA.release();
+        mGrey.release();
     }
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         mRGBA = inputFrame.rgba();
-        mRGBAT = mRGBA.t();
+        mGrey = inputFrame.gray();
+        /*mRGBAT = mRGBA.t();
+
         //Core.flip(mRGBA.t(),  mRGBAT,1); ----> Distorce l'immagine, quindi non serve
         Core.flip(mRGBA,  mRGBAT,1);
         Imgproc.resize(mRGBAT, mRGBAT, mRGBA.size());
 
         // releasing what's not anymore needed
         mRGBA.release();
-
         return mRGBAT;
+    */
+        //Face detection
+        MatOfRect faceDetections = new MatOfRect();
+        faceDetector.detectMultiScale(mRGBA, faceDetections);
+
+        for(Rect rect: faceDetections.toArray()) {
+            Imgproc.rectangle(
+                    mRGBA,
+                        new Point(rect.x, rect.y),
+                        new Point(rect.x + rect.width, rect.y + rect.height),
+                        new Scalar(255,0,0));
+        }
+        return mRGBA;
     }
 
     @Override
@@ -163,5 +165,56 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
             OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this,baseLoaderCallback);
         }
     }
+
+    private BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(MainActivity.this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch(status) {
+                case BaseLoaderCallback.SUCCESS: {
+                    InputStream is= getResources().openRawResource(R.raw.haarcascade_frontalface_alt2);
+
+                    File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+                    cascFile = new File(cascadeDir, "haarcascade_frontalface_alt2.xml");
+
+                    FileOutputStream fos = null;
+                    try {
+                        fos = new FileOutputStream(cascFile);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
+                    byte[] buffer = new byte[4096];
+                    int bytesRead;
+
+                       try {
+                            while (((bytesRead = is.read(buffer)) != -1)) {
+                                fos.write(buffer, 0, bytesRead);
+                            }
+                            is.close();
+                            fos.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    faceDetector = new CascadeClassifier(cascFile.getAbsolutePath());
+                    if(faceDetector.empty()) {
+                        faceDetector=null;
+                    } else {
+                        cascadeDir.delete();
+                    }
+
+                    javaCameraView.enableView();
+
+                    break;
+                }
+                default: {
+                    super.onManagerConnected(status);
+                    break;
+                }
+            }
+            super.onManagerConnected(status);
+        }
+    };
 
 }
