b0efdbd17494eef6c3b7223892ea2f7c163ac7f7
marzna <50882520+marzna@users.noreply.github.com>
2021-12-13 12:05:10 +0100

    Aggiunta di commenti

--- PeppeRecycle/app/src/main/java/com/example/pepperecycle/CommonUtils.java
+++ PeppeRecycle/app/src/main/java/com/example/pepperecycle/CommonUtils.java
@@ -2,34 +2,35 @@ package com.example.pepperecycle;
 
 import android.app.Activity;
 import android.app.Dialog;
-import android.content.Context;
-import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+/* Classe contenente metodi comuni per tutto il gioco.
+    Al momento, sono presenti solamente:
+        - showDialogExit(...)
+        - showDialog(...)
+ */
 public class CommonUtils {
     private static final String TAG = "CommonUtils";
-//    static Dialog dialog;
 
+    //mostra finestra di dialogo che chiede conferma quando si vuole chiudere il gioco
     public static void showDialogExit(Activity activity) { //desc sarà il contenuto della finestra di dialogo
         Dialog dialog = new Dialog(activity);
         dialog.setContentView(R.layout.dialog_exit_confirm_layout);
         dialog.getWindow().setBackgroundDrawableResource(R.drawable.bg_window);
-        Log.e(TAG, "Entrata nella showDialog");
-        //dialog.getWindow().setBackgroundDrawableResource(R.drawable.bg_window);
+        //Log.d(TAG, "showDialogExit");
         dialog.getWindow().getAttributes().windowAnimations = R.style.animation;
 
-        ImageButton dialogButtonCloseYes = (ImageButton) dialog.findViewById(R.id.dialogButtonCloseYes);
-        ImageButton dialogButtonCloseNo = (ImageButton) dialog.findViewById(R.id.dialogButtonCloseNo);
+        ImageButton dialogButtonCloseYes = dialog.findViewById(R.id.dialogButtonCloseYes);
+        ImageButton dialogButtonCloseNo = dialog.findViewById(R.id.dialogButtonCloseNo);
 
         dialogButtonCloseYes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*activity.finish();*/
-                activity.finishAffinity(); // Close all activites
-                System.exit(0);  // Releasing resources
+                activity.finishAffinity(); // Chiusura di tutte le activity
+                System.exit(0);  // Rilascio risorse
             }
 
         });
@@ -40,25 +41,23 @@ public class CommonUtils {
             }
 
         });
-        Log.e(TAG, "Prima di dialog.show");
         dialog.show();
-        Log.e(TAG, "Dopo dialog.show");
 
     }
 
-
+    /* Mostra finestra di dialogo più generica. Attualmente, è utilizzata solo
+     * per la spiegazione del turno corrente quando si preme sul bottone di "?"
+     */
     public static void showDialog(Activity activity, String mex) { //desc sarà il contenuto della finestra di dialogo
         Dialog dialog = new Dialog(activity);
         dialog.setContentView(R.layout.dialog_tutorial_layout);
-        Log.e(TAG, "Entrata nella showDialog");
+        //Log.d(TAG, "showDialog");
         dialog.getWindow().setBackgroundDrawableResource(R.drawable.bg_window);
         dialog.getWindow().getAttributes().windowAnimations = R.style.animation;
-        TextView textViewDialogTutorial = (TextView) dialog.findViewById(R.id.textViewDialogTutorial);
-        ImageButton dialogButtonClose = (ImageButton) dialog.findViewById(R.id.dialogButtonClose);
-//TODOhttps://youtu.be/vDAO7H5w4_I
-        Log.e(TAG, "Prima di settext");
+        TextView textViewDialogTutorial = dialog.findViewById(R.id.textViewDialogTutorial);
+        ImageButton dialogButtonClose = dialog.findViewById(R.id.dialogButtonClose);
+
         textViewDialogTutorial.setText(mex);
-        Log.e(TAG, "Dopo settext");
 
         dialogButtonClose.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,47 +67,7 @@ public class CommonUtils {
 
         });
 
-        Log.e(TAG, "Prima di dialog.show");
         dialog.show();
-        Log.e(TAG, "Dopo dialog.show");
 
     }
-
-    /*public static void showScore(Activity activity) {
-//TODO
-    }*/
-
-
-
 }
-
-/*public void showDialogExit(Activity activity, Dialog dialog) { //desc sarà il contenuto della finestra di dialogo
-
-        dialog.setContentView(R.layout.dialog_exit_confirm_layout);
-        dialog.getWindow().setBackgroundDrawableResource(R.drawable.bg_window);
-        Log.e(TAG, "Entrata nella showDialog");
-        //dialog.getWindow().setBackgroundDrawableResource(R.drawable.bg_window);
-        dialog.getWindow().getAttributes().windowAnimations = R.style.animation;
-
-        ImageButton dialogButtonCloseYes = (ImageButton) dialog.findViewById(R.id.dialogButtonCloseYes);
-        ImageButton dialogButtonCloseNo = (ImageButton) dialog.findViewById(R.id.dialogButtonCloseNo);
-
-        dialogButtonCloseYes.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                activity.finish();
-            }
-
-        });
-        dialogButtonCloseNo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
-            }
-
-        });
-        Log.e(TAG, "Prima di dialog.show");
-        dialog.show();
-        Log.e(TAG, "Dopo dialog.show");
-
-    }*/
